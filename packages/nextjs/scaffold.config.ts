069f0ae095ff19093fd08d7d89098026fd791f8f
ChainsQueen.eth <glam_younger_0e@icloud.com>
2025-08-23 14:25:44 +0100

    chore(nextjs): target Intuition Testnet in scaffold.config  Feedback submitted

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,3 +1,4 @@
+import { intuitionTestnet } from "./intuition";
 import * as chains from "viem/chains";
 
 export type BaseConfig = {
@@ -15,7 +16,7 @@ export const DEFAULT_ALCHEMY_API_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.hardhat],
+  targetNetworks: [intuitionTestnet],
   // The interval at which your front-end polls the RPC servers for new data (it has no effect if you only target the local network (default is 4000))
   pollingInterval: 30000,
   // This is ours Alchemy's default API key.
@@ -34,7 +35,7 @@ const scaffoldConfig = {
   // It's recommended to store it in an env variable:
   // .env.local for local testing, and in the Vercel/system env config for live apps.
   walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b534d0ff9d794f19a901d64",
-  onlyLocalBurnerWallet: true,
+  onlyLocalBurnerWallet: false,
 } as const satisfies ScaffoldConfig;
 
 export default scaffoldConfig;
